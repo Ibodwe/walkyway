57904a2f9b01adc7cb0c29f2d50df73882ef1d20
Minju Lee <aamj0226@gmail.com>
2020-10-10 15:56:47 +0900

    change package name

--- app/src/main/java/com/example/walkway/walkingwalkway/StartWalkwayActivity.kt
+++ app/src/main/java/com/example/walkway/walkingwalkway/StartWalkwayActivity.kt
@@ -1,12 +1,12 @@
-package com.example.walkway.walkwaywalking
+package com.example.walkway.walkingwalkway
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.walkway.R
 
-class FinishWalkwayActivity : AppCompatActivity() {
+class StartWalkwayActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_finish_walkway)
+        setContentView(R.layout.activity_start_walkway)
     }
 }
