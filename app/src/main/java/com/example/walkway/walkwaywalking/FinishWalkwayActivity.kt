f99393d03ce70abc5dc5804e0138ba40f39df909
Minju Lee <aamj0226@gmail.com>
2020-10-10 15:41:11 +0900

    upload walkwaywalking

--- app/src/main/java/com/example/walkway/walkwaywalking/FinishWalkwayActivity.kt
+++ app/src/main/java/com/example/walkway/walkwaywalking/FinishWalkwayActivity.kt
@@ -2,6 +2,7 @@ package com.example.walkway.walkwaywalking
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.walkway.R
 
 class FinishWalkwayActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/walkway/walkwaywalking/StopWalkwayActivity.kt
+++ app/src/main/java/com/example/walkway/walkwaywalking/StopWalkwayActivity.kt
@@ -14,22 +14,22 @@ class StopWalkwayActivity : AppCompatActivity() {
     }
     
     // '산책을 중단하시겠습니까?' alert 창 띄우기
-    private fun showSettingPopup() {
-        val inflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-        val view = inflater.inflate(R.layout.alert_popup, null)
-        val textView: TestView = view.findViewById(R.id.textView)
-        textView.text = "산책을 중단하시겠습니까?"
-
-        val alertDialog = AlertDialog.Builder(this)
-            .setTitle("AlertDialog Exam")
-            .setPositiveButton("예") { dialog, which ->
-                Toast.makeText(applicationContext, "산책이 중단되었습니다.", Toast.LENGTH_SHORT).show()
-            }
-            .setNeutralButton("아니오", null)
-            .create()
-
-        alertDialog.setView(view)
-        alertDialog.show()
-    }
+//    private fun showSettingPopup() {
+//        val inflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+//        val view = inflater.inflate(R.layout.alert_popup, null)
+//        val textView: TestView = view.findViewById(R.id.textView)
+//        textView.text = "산책을 중단하시겠습니까?"
+//
+//        val alertDialog = AlertDialog.Builder(this)
+//            .setTitle("AlertDialog Exam")
+//            .setPositiveButton("예") { dialog, which ->
+//                Toast.makeText(applicationContext, "산책이 중단되었습니다.", Toast.LENGTH_SHORT).show()
+//            }
+//            .setNeutralButton("아니오", null)
+//            .create()
+//
+//        alertDialog.setView(view)
+//        alertDialog.show()
+//    }
 
 }
